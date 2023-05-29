f42efaaefdad5a1c39232d76f9ccc146312813bd
mashi-mashi <mashi.07.st@gmail.com>
2023-05-29 23:30:40 +0900

    Add applyevent

--- src/post.ts
+++ src/post.ts
@@ -1,10 +1,61 @@
 import { generateId } from './uuid';
 
+type PostEvent = PostCreatedEvent | PostUpdatedEvent | PostPublishedEvent;
+
+type PostCreatedEvent = {
+  type: 'PostCreatedEvent';
+  payload: {
+    title: string;
+    content: string;
+  };
+};
+
+type PostUpdatedEvent = {
+  type: 'PostUpdatedvent';
+  payload: {
+    title?: string;
+    content?: string;
+  };
+};
+
+type PostPublishedEvent = {
+  type: 'PostPublishedEvent';
+  payload: {
+    postId: string;
+  };
+};
+
 class Post {
   public readonly id: string;
 
-  constructor(public readonly title: string, public readonly content: string) {
+  private constructor(public readonly title: string, public readonly content: string) {
     this.id = generateId();
+  }
+
+  private copyWith(post: Partial<Post>) {
+    return new Post(post.title || this.title, post.content || this.content);
+  }
+
+  applyEvent(event: PostEvent): Post {
+    switch (event.type) {
+      case 'PostCreatedEvent': {
+        const createdEvent = { ...event } as PostCreatedEvent;
+        return this.copyWith({
+          title: createdEvent.payload.title,
+          content: createdEvent.payload.content,
+        });
+      }
+      case 'PostUpdatedvent': {
+        const updatedEvent = event as PostUpdatedEvent;
+        return this.copyWith({
+          title: updatedEvent.payload.title,
+          content: updatedEvent.payload.content,
+        });
+      }
+      case 'PostPublishedEvent': {
+        return this.copyWith({});
+      }
+    }
   }
 }
 

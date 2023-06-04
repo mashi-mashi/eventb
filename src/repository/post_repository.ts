4357393f1713b7379a938c256fc54cf003534231
mashi-mashi <mashi.07.st@gmail.com>
2023-06-04 15:57:02 +0900

    Refactor types

--- src/repository/post_repository.ts
+++ src/repository/post_repository.ts
@@ -1,14 +1,23 @@
+import { EventSourcedEntity, EventType } from '../core/event';
+import { BasePost } from '../model/base_post';
 import { Post } from '../model/post';
+import { PostEvent } from '../model/post_event';
 import { PublishedPost } from '../model/published_post';
-import { PostSerializerType, postSerializer } from '../serializer/post_serializer';
-import { JsonType } from '../serializer/serializable';
+import { postSerializer } from '../serializer/post_serializer';
+import { JsonType, Serializable } from '../serializer/serializable';
 
 type DataBaseType = {
   store: <T>(any: JsonType<T>) => Promise<void>;
   get: <T>(id: string) => Promise<JsonType<T>>;
 };
 
-const postRepository = <T>(f: (db: DataBaseType, serializer: PostSerializerType) => Promise<T>) => {
+export type RepositoryType<T extends EventSourcedEntity<E, T>, E extends EventType> = (
+  f: (db: DataBaseType, serializer: Serializable<T, E>) => Promise<T>,
+) => Promise<T>;
+
+type PostRepositoryType = RepositoryType<BasePost, PostEvent>;
+
+const postRepository: PostRepositoryType = (f) => {
   return f(
     {
       store: async (any) => {
@@ -23,7 +32,7 @@ const postRepository = <T>(f: (db: DataBaseType, serializer: PostSerializerType)
   );
 };
 
-export async function createPost(post: Post | PublishedPost) {
+export async function createPost(post: BasePost) {
   return await postRepository(async (db, serializer) => {
     await db.store(serializer.serialize(post));
     await Promise.all(serializer.serializeEvents(post.events).map(db.store));

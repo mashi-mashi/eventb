12ed0c10ad0c78f54d5352340e2b4beed144e0ce
mashi-mashi <mashi.07.st@gmail.com>
2023-06-06 00:38:16 +0900

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,20 @@
-import { match } from 'ts-pattern';
-import { createPost } from './repository/post_repository';
-import { Post } from './model/post/post';
-import { UserIdType } from './model/user/base_user';
-import { CreatePostUseCase, withAuthor } from './application/create_post_use_case';
+import { CreatePostUseCase } from './application/create_post_use_case';
+import { withAuthor } from './application/use_case';
+import { storePost } from './repository/post_repository';
 
 (async () => {
-  // const s = await createPost(
-  //   Post.create({ authorId: 'author' as UserIdType, title: 'title', content: 'content' })
-  //     .update({
-  //       title: 'new title1',
-  //     })
-  //     .update({
-  //       title: 'new title2',
-  //     })
-  //     .update({
-  //       title: 'new title3',
-  //     })
-  //     .update({
-  //       title: 'new title4',
-  //     })
-  //     .publish(new Date())
-  //     .unpublish(),
-  // );
-
-  // match(s)
-  //   .with({ kind: 'Post' }, (post) => {
-  //     console.log('post', post);
-  //     Post.of(post).lastEvent?.type;
-  //   })
-  //   .with({ kind: 'PublishedPost' }, (publishedPost) => {
-  //     console.log('PublishedPost!!!!!!!', publishedPost);
-  //   })
-  //   .exhaustive();
-
   await withAuthor(
     {},
-    new CreatePostUseCase(),
-  )({ title: 'title', content: 'content' }).then((post) => {
-    console.log('CreatePostUseCase', post);
+    new CreatePostUseCase(storePost),
+  )({ title: 'title', content: 'content' }).then((result) => {
+    console.log(
+      'CreatePostUseCase',
+      result.when({
+        ok: (value) => {
+          return { value, message: 'ok' };
+        },
+        err: (error) => ({ message: error.message }),
+      }),
+    );
   });
 })();

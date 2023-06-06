5b8d11332cf627200022df09d6d7831906c8a089
mashi-mashi <mashi.07.st@gmail.com>
2023-06-06 22:09:13 +0900

    Impl simple DI container

---
+++ src/container.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+import { AnyType } from './lib/type';
+import { PostRepositoryOnPrisma } from './repository/post_repository';
+import { postSerializer } from './serializer/post_serializer';
+
+interface Constructor<T> {
+  new (...args: AnyType[]): T;
+}
+
+export class Container {
+  private static instance: Container;
+  private services = new Map();
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private constructor() {}
+
+  static getInstance(): Container {
+    if (!Container.instance) {
+      Container.instance = new Container();
+    }
+    return Container.instance;
+  }
+
+  public register<T>(token: string, constructor: Constructor<T>, ...args: AnyType[]): void {
+    if (this.services.has(token)) {
+      throw new Error(`Token ${token} is already registered`);
+    }
+    this.services.set(token, new constructor(...args));
+  }
+
+  public resolve<T>(token: string): T {
+    if (!this.services.has(token)) {
+      throw new Error(`Token ${token} is not registered`);
+    }
+    return this.services.get(token) as T;
+  }
+}
+
+const container = Container.getInstance();
+
+container.register('Prisma', PrismaClient);
+container.register('PostRepository', PostRepositoryOnPrisma, container.resolve('Prisma'), postSerializer);
+
+export { container };

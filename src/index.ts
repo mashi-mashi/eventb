153f743c9b5dd7ba3611faa0e5ac902790f8a6b5
mashi-mashi <mashi.07.st@gmail.com>
2023-06-10 16:41:24 +0900

    Add fastify

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import Fastify, { FastifyInstance } from 'fastify';
 import { CreatePostUseCase } from './application/command/create_post_use_case';
 import { withAuthor } from './application/command/use_case';
 import { container } from './container';
@@ -5,20 +6,85 @@ import { UserIdType } from './model/user/user';
 
 const withAuhorCreatePost = withAuthor(container.resolve(CreatePostUseCase));
 
-(async () => {
-  const d = await withAuhorCreatePost({
-    authorId: '5b4d1511-6915-4040-bad1-1b212bb7a637' as UserIdType,
-    input: {
-      title: 'test',
-      content: 'test',
+const server: FastifyInstance = Fastify({ logger: true });
+
+server.get(
+  '/ping',
+  {
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            pong: {
+              type: 'string',
+            },
+          },
+        },
+      },
     },
-  });
+  },
+  async () => {
+    return { pong: 'it worked!' };
+  },
+);
 
-  d.when({
-    ok: (v) => console.log('ok!!!!', v),
-    err: (e) => console.log(e),
-  });
+server.get(
+  '/posts/:id',
+  {
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            ok: { type: 'boolean' },
+            value: {
+              type: 'object',
+              additionalProperties: true, // なんでもありになるので注意
+            },
+            error: {
+              type: 'object',
+              additionalProperties: true, // なんでもありになるので注意
+            },
+          },
+        },
+      },
+    },
+  },
+  async (req, reply) => {
+    const d = await withAuhorCreatePost({
+      authorId: '5b4d1511-6915-4040-bad1-1b212bb7a6371' as UserIdType,
+      //authorId: '5b4d1511-6915-4040-bad1-1b212bb7a637' as UserIdType,
+      input: {
+        title: 'test',
+        content: 'test',
+      },
+    });
+    return d.when({
+      ok: (v) => {
+        return reply.status(200).send({
+          ok: true,
+          value: v,
+        });
+      },
+      err: (e) => {
+        server.log.error(e);
+        return reply.status(500).send({ ok: false, error: { message: e.message } });
+      },
+    });
+  },
+);
 
-  // const q = await container.resolve(PostQuery).listByAuthorId('5b4d1511-6915-4040-bad1-1b212bb7a637' as UserIdType);
-  // console.log(q);
-})();
+const start = async () => {
+  try {
+    await server.listen({ port: 3000 });
+
+    const address = server.server.address();
+    const port = typeof address === 'string' ? address : address?.port;
+    console.log(`server listening on ${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+start();
